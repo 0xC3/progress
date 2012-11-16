3fe5040f76d2f8c73ccf74866f9e0d2a773cbe0a
ukn <re0x00@gmail.com>
2012-11-16 16:26:37 +0100

    progress: Updated readme and applied go fmt.

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ progress
 ========
 
 This package implements the backend functions for handling progress bars.
+
+public domain
+-------------
+
+This code is hereby released into the *[public domain][]*.
+
+[public domain]: https://creativecommons.org/publicdomain/zero/1.0/
--- barcli/winsize.go
+++ barcli/winsize.go
@@ -5,13 +5,13 @@ import "unsafe"
 
 // winSize contains the dimentions of a terminal.
 type winSize struct {
-   Row, Col       uint16
-   XPixel, YPixel uint16
+	Row, Col       uint16
+	XPixel, YPixel uint16
 }
 
 // getWinSize returns a winSize for the current terminal.
 func getWinSize() winSize {
-   ws := winSize{}
-   syscall.Syscall(syscall.SYS_IOCTL, uintptr(0), uintptr(syscall.TIOCGWINSZ), uintptr(unsafe.Pointer(&ws)))
-   return ws
+	ws := winSize{}
+	syscall.Syscall(syscall.SYS_IOCTL, uintptr(0), uintptr(syscall.TIOCGWINSZ), uintptr(unsafe.Pointer(&ws)))
+	return ws
 }
